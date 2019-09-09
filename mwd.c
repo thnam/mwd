72a69720121a16084db279e782095c7f3aeefbf1
nam <thnam.me@gmail.com>
2019-09-09 12:17:27 -0400

    time benchmark

--- mwd.c
+++ mwd.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #include "algo.h"
 #include "vector.h"
 
 Vector * ReadWF(const char * filename);
+long int getMicrotime(){
+  struct timeval currentTime;
+  gettimeofday(&currentTime, NULL);
+  return currentTime.tv_sec * (int)1e6 + currentTime.tv_usec;
+}
 
 int main(int argc, char *argv[]) {
+  long int start = getMicrotime();
   Vector * wf = ReadWF("samples/purdue_full_wf0.csv");
+  long int stop = getMicrotime();
+  printf("Reading time %ld usec = %ld ms\n", (stop - start), (stop - start)/1000);
 
+  start = getMicrotime();
   Vector * mwd = MWD(wf, 0.999993, 6000, 600);
+  stop = getMicrotime();
+  printf("MWD time %ld usec = %ld ms\n", (stop - start), (stop - start)/1000);
   for (uint32_t i = 0; i < mwd->size; ++i) {
-    printf("%.9lf\n", mwd->data[i]);
+    /* printf("%.9lf\n", mwd->data[i]); */
   }
 
   VectorFree(mwd);
